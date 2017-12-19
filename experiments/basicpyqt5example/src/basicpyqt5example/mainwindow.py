3c4c17abc4629e4fd4acdd8d4d8ca229ebd00b1f
Kyle Altendorf <kyle.altendorf@epcpower.com>
2017-12-18 22:20:52 -0500

    Rename {open,close}_system_info()

--- experiments/basicpyqt5example/src/basicpyqt5example/mainwindow.py
+++ experiments/basicpyqt5example/src/basicpyqt5example/mainwindow.py
@@ -11,16 +11,18 @@ class MainWindow:
             pathlib.Path(__file__).parents[0] / 'mainwindow.ui',
         )
 
-        self.ui.actionSystem_Info.triggered.connect(lambda: self.system_info())
+        self.ui.actionSystem_Info.triggered.connect(
+            lambda: self.open_system_info(),
+        )
         self.system_info_dialogs = []
 
-    def system_info(self):
+    def open_system_info(self):
         dialog = basicpyqt5example.systeminfo.SystemInfoDialog()
-        dialog.ui.finished.connect(lambda: self.system_info_close(dialog))
+        dialog.ui.finished.connect(lambda: self.close_system_info(dialog))
         dialog.ui.show()
         self.system_info_dialogs.append(dialog)
 
-    def system_info_close(self, dialog):
+    def close_system_info(self, dialog):
         self.system_info_dialogs.remove(dialog)
         print(
             'removed {}, still have {}'.format(dialog, self.system_info_dialogs)
